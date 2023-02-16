c1a9efcd9f205cbe5f96d04b3c935df988e765f0
Ahmed Mahmoud <ahmedmahmoud@linux200.csc.uvic.ca>
2023-02-15 18:00:55 -0800

    parent 8300b894a56e799bffae8ea1e4483e3c299545f1 author Ahmed Mahmoud <ahmedmahmoud@linux200.csc.uvic.ca> 1676512855 -0800 committer Ahmed Mahmoud <ahmedmahmoud@linux201.csc.uvic.ca> 1676537998 -0800
    cleaning up sud2sat: reading file and  row and column
    
    Creating a class for the basic solver functions and using it in all other files
    
    Merging all sat2sud files to call the same function from the original 1
    
    * modifying the sud to sat to accept 0 and . for unkown
    * cleaning a bit in sat2sud
    * adding the import line for python3 bash

--- sat2sud.py
+++ sat2sud.py
@@ -1,40 +1,37 @@
+#! /usr/bin/env python3
 import sys
 
-if __name__ == '__main__':
 
-    def getNumber(n):
-        return (n-1)%9+1
-
-    fileName = sys.argv[1]
+def main(file_name):
     number = []
-    with open(fileName, 'r') as f:
-        for line in f:
-            if 'UNSAT' in line:
-                open('solution.txt', 'w').close()
-                f = open('solution.txt', 'w')
+    with open(file_name, 'r') as f:
+        context = f.read()
+        number, unsolvable = [], False
+        if 'UNSAT' in context:
+            unsolvable = True
+        else:
+            number = [l.split(" ") for l in context.split("\n")]
+
+    if not number or unsolvable:    
+            with open('solution.txt', 'w+') as f:
                 f.write("The sudoku given is unsatisfiable.\n")
-                f.close()
-                break
-            else:
-                number.append(line.split(" "))
+            print("The sudoko is unsolvable")
 
-    if len(number) > 0:
-        solution = []
-        for i in range(len(number[1])):
-            if int(number[1][i]) > 0:
-                numberToPrint = getNumber(int(number[1][i]))
-                solution.append(numberToPrint)
+    solution = []
+    for n in number[1]:
+        if int(n) > 0:
+            num_to_print = (int(n) - 1) % 9 + 1
+            solution.append(num_to_print)
 
-        f = open("solution.txt", "w+")
-
+    with open("solution.txt", "w+") as f:
         for i in range(len(solution)):
             if i != 0 and i % 9 == 0:
                 f.write("\n")
             if i % 9 != 0 and i % 3 == 0:
                 f.write(" ")
             f.write("%d"% (solution[i]))
-
         f.write("\n")
-        f.close()
-    else:
-        print("The sudoku given is unsatisfiable.")
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    main(file_name)
--- sud2sat.py
+++ sud2sat.py
@@ -1,74 +1,80 @@
+#! /usr/bin/env python3
 import sys
+import re
 
-if __name__ == '__main__':
-
-    def var(row, column, value):
+class Sud2Sat():
+    def __init__(self, file_name : str) -> None:
+        with open(file_name, "r") as f:
+            self.sudoco = re.sub(r"[\n\t\s]*", "", f.read()) #remove spaces, tabs and new lines
+    
+    def var(self, row, column, value)->int:
+        # this function return a representation of specific value in specific row and column
         return 81*(row-1)+9*(column-1)+(value-1)+1
 
-    # Read sudoku and add the given clues as clauses
-    fileName = sys.argv[1]
-    clauses = []
-    digits = {'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
-    count = 0
-    with open(fileName, "r") as f:
-        row = 0
-        column = 0
-        while 1:
-            char = f.read(1)   
-            if not char:
-                break
-            if char in digits.keys():
-                clauses.append([var(row+1, column+1, int(char))])
-            if not char.isspace() or '\n' not in char:
-                count = count + 1
-                column = column + 1
-            if count == 9:
-                row = row + 1
-                column = 0
-                count = 0
+    def _get_clues_clauses(self) -> list:
+        # Read sudoku and add the given clues as clauses
+        clauses = []
+        for row in range(9):
+            for col in range(9):
+                curr_char = self.sudoco[row*9 + col]
+                if curr_char.isnumeric() and int(curr_char): # if number and not zero
+                    clauses.append([self.var(row+1, col+1, int(curr_char))])
+        return clauses
     
-    # Every cell contains at least one number
-    for row in range(1, 10):
-        for column in range(1, 10):
-            clauses.append([var(row, column, value) for value in range(1, 10)])
+    def _get_rows_cols_clauses(self):
+        # Each number appears at most once in every row and every column
+        clauses = []
+        for i in range(1, 10):
+            for k in range(1, 10):
+                for j in range(1, 9):
+                    for l in range(j+1, 10):
+                        clauses.append([-self.var(i, j, k), -self.var(i, l, k)]) #rows
+                        clauses.append([-self.var(j, i, k), -self.var(l, i, k)]) #columns
+        return clauses
 
-    # Each number appears at most once in every row
-    for i in range(1, 10):
+    def _get_boxes_clauses(self):
+        # Each number appears at most one in every 3x3 sub-grid
+        clauses = []
         for k in range(1, 10):
-            for j in range(1, 9):
-                for l in range(j+1, 10):
-                    clauses.append([-var(i, j, k), -var(i, l, k)])
+            for a in range(0, 3):
+                for b in range(0, 3):
+                    for u in range(1, 4):
+                        for v in range(1, 3):
+                            for w in range(v+1, 4):
+                                clauses.append([-self.var(3*a+u,3*b+v,k), -self.var(3*a+u,3*b+w,k)])
+                    for u in range(1, 3):
+                        for v in range(1, 4):
+                            for w in range(u+1, 4):
+                                for t in range(1, 4):
+                                    clauses.append([-self.var(3*a+u,3*b+v,k), -self.var(3*a+w,3*b+t, k)])
+        return clauses
+        
+    def get_clauses(self):
+        clauses = []
 
-    # Each number appears at most once in every column
-    for j in range(1, 10):
-        for k in range(1, 10):
-            for i in range(1, 9):
-                for l in range(i+1, 10):
-                    clauses.append([-var(i, j, k), -var(l, j, k)])
+        # Every cell contains at least one number
+        for row in range(1, 10):
+            for column in range(1, 10):
+                clauses.append([self.var(row, column, value) for value in range(1, 10)])
+        
+        clauses += self._get_clues_clauses()
+        clauses += self._get_rows_cols_clauses()
+        clauses += self._get_boxes_clauses()
 
-    # Each number appears at most one in every 3x3 sub-grid
-    for k in range(1, 10):
-        for a in range(0, 3):
-            for b in range(0, 3):
-                for u in range(1, 4):
-                    for v in range(1, 3):
-                        for w in range(v+1, 4):
-                            clauses.append([-var(3*a+u,3*b+v,k), -var(3*a+u,3*b+w,k)])
-                for u in range(1, 3):
-                    for v in range(1, 4):
-                        for w in range(u+1, 4):
-                            for t in range(1, 4):
-                                clauses.append([-var(3*a+u,3*b+v,k), -var(3*a+w,3*b+t, k)])
+        return clauses
 
-    # Create the CNF file for minisat
-    f = open("puzzle.cnf", "w+")
+def main(sudo_file):
+    sudoco = Sud2Sat(sudo_file)
+    clauses = sudoco.get_clauses() #clauses from basic sud2sat
 
-    # Print the header line
-    f.write("p cnf %d %d\n" % (729, len(clauses)))
+    with open("puzzle.cnf", "w+") as f:
+        # Print the header line
+        f.write("p cnf %d %d\n" % (729, len(clauses)))
 
-    # Print the clauses
-    for c in clauses:
-        f.write(" ".join([str(l) for l in c])+" 0\n")
+        # Print the clauses
+        for c in clauses:
+            f.write(" ".join([str(l) for l in c])+" 0\n")
 
-    # Close file created
-    f.close()
+if __name__ == '__main__':
+    sudo_file = sys.argv[1]
+    main(sudo_file)
