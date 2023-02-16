201dcd726887b06ffba6a6e492c21676f2c7ae8d
Ahmed Mahmoud <ahmedmahmoud@linux200.csc.uvic.ca>
2023-02-15 18:00:55 -0800

    cleaning up sud2sat: reading file and  row and column
    Creating a class for the basic solver functions and using it in all other files
    
    Merging all sat2sud files to call the same function from the original 1

--- sud2sat.py
+++ sud2sat.py
@@ -1,74 +1,43 @@
 import sys
 
-if __name__ == '__main__':
 
-    def var(row, column, value):
-        return 81*(row-1)+9*(column-1)+(value-1)+1
+def getNumber(n):
+    return (n-1)%9+1
 
-    # Read sudoku and add the given clues as clauses
-    fileName = sys.argv[1]
-    clauses = []
-    digits = {'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
-    count = 0
-    with open(fileName, "r") as f:
-        row = 0
-        column = 0
-        while 1:
-            char = f.read(1)   
-            if not char:
-                break
-            if char in digits.keys():
-                clauses.append([var(row+1, column+1, int(char))])
-            if not char.isspace() or '\n' not in char:
-                count = count + 1
-                column = column + 1
-            if count == 9:
-                row = row + 1
-                column = 0
-                count = 0
+def main(input_file: str): 
+    with open(input_file, 'r') as f:
+        number, unsolvable = [], False
+        cntxt = f.read()
+        if "UNSAT" in cntxt:
+            unsolvable = True
+        else:
+            number = [line.split(" ") for line in cntxt.split("\n")]
     
-    # Every cell contains at least one number
-    for row in range(1, 10):
-        for column in range(1, 10):
-            clauses.append([var(row, column, value) for value in range(1, 10)])
+    if not number or unsolvable:
+        open('solution.txt', 'w').close()
+        with open('solution.txt', 'w') as f:
+            f.write("The sudoku given is unsatisfiable.\n")
+        print("The sudoku given is unsatisfiable.")
+        return
 
-    # Each number appears at most once in every row
-    for i in range(1, 10):
-        for k in range(1, 10):
-            for j in range(1, 9):
-                for l in range(j+1, 10):
-                    clauses.append([-var(i, j, k), -var(i, l, k)])
+    solution = []
+    for i in range(len(number[1])):
+        if int(number[1][i]) > 0:
+            numberToPrint = getNumber(int(number[1][i]))
+            solution.append(numberToPrint)
 
-    # Each number appears at most once in every column
-    for j in range(1, 10):
-        for k in range(1, 10):
-            for i in range(1, 9):
-                for l in range(i+1, 10):
-                    clauses.append([-var(i, j, k), -var(l, j, k)])
+    with open("solution.txt", "w+") as f:
+        for i in range(len(solution)):
+            if i != 0 and i % 9 == 0:
+                f.write("\n")
+            if i % 9 != 0 and i % 3 == 0:
+                f.write(" ")
+            f.write("%d"% (solution[i]))
 
-    # Each number appears at most one in every 3x3 sub-grid
-    for k in range(1, 10):
-        for a in range(0, 3):
-            for b in range(0, 3):
-                for u in range(1, 4):
-                    for v in range(1, 3):
-                        for w in range(v+1, 4):
-                            clauses.append([-var(3*a+u,3*b+v,k), -var(3*a+u,3*b+w,k)])
-                for u in range(1, 3):
-                    for v in range(1, 4):
-                        for w in range(u+1, 4):
-                            for t in range(1, 4):
-                                clauses.append([-var(3*a+u,3*b+v,k), -var(3*a+w,3*b+t, k)])
+        f.write("\n")
+    
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    main(input_file)
 
-    # Create the CNF file for minisat
-    f = open("puzzle.cnf", "w+")
 
-    # Print the header line
-    f.write("p cnf %d %d\n" % (729, len(clauses)))
-
-    # Print the clauses
-    for c in clauses:
-        f.write(" ".join([str(l) for l in c])+" 0\n")
-
-    # Close file created
-    f.close()
