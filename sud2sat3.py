c3d9b81ec861204e582aaa8f73b9b6faa5a9d988
Davin Norgard <56693942+DavinCNorgard@users.noreply.github.com>
2023-02-21 22:26:28 -0800

    Update sud2sat3.py

--- sud2sat3.py
+++ sud2sat3.py
@@ -1,30 +1,37 @@
 #! /usr/bin/env python3
 import sys
 from sud2sat import Sud2Sat
+import sud2sat
 
 
 def main(sudo_file):
-    sudoco = Sud2Sat(sudo_file)
-    clauses = sudoco.get_clauses() #clauses from basic sud2sat
+    sudoku = Sud2Sat(sudo_file, True)
+    sudokuRules = sud2sat.get_sudoku_rules() 
+    clueClauses = sudoku._get_clues_clauses()
+    clauses = sudokuRules + clueClauses
 
     # There is at most one number in each cell (efficient encoding)
     for i in range(1, 10):
         for j in range(1, 10):
             for k in range(1, 9):
                 for l in range(k+1, 10):
-                    clauses.append([-sudoco.var(i, j, k), -sudoco.var(i, j, l)])
+                    clauses.append([-sudoku.var(i, j, k), -sudoku.var(i, j, l)])
 
-    # Every number appears at least once in each row and each column
+    # Every number appears at least once in each row
     for i in range(1, 10):
         for k in range(1, 10):
-            clauses.append([sudoco.var(i, j, k) for j in range(1, 10)]) # row
-            clauses.append([sudoco.var(j, i, k) for j in range(1, 10)]) # column
+            clauses.append([sudoku.var(i, j, k) for j in range(1, 10)])
+
+    # Every number appears at least once in each column
+    for j in range(1, 10):
+        for k in range(1, 10):
+            clauses.append([sudoku.var(i, j, k) for i in range(1, 10)])
 
     # Every number appears at least once in each subgrid
     for k in range(1, 10):
         for a in range(0, 3):
             for b in range(0, 3):
-                clauses.append([sudoco.var(3*a+u, 3*b+v, k) for u in range(1, 4) for v in range(1, 4)])
+                clauses.append([sudoku.var(3*a+u, 3*b+v, k) for u in range(1, 4) for v in range(1, 4)])
 
     with open("puzzle.cnf", "w+") as f:
         # Print the header line
